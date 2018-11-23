587bd7c268a303c1d50da416fd29ccf35a79ce45
lz <774138702@qq.com>
2018-11-23 12:22:01 +0800

    增加背景

--- app/src/main/java/com/example/neaium/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/neaium/coolweather/WeatherActivity.java
@@ -2,6 +2,7 @@ package com.example.neaium.coolweather;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.IBinder;
 import android.preference.PreferenceManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -11,11 +12,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.neaium.coolweather.gson.Forecast;
 import com.example.neaium.coolweather.gson.Weather;
 import com.example.neaium.coolweather.service.AutoUpdateService;
@@ -44,6 +47,7 @@ public class WeatherActivity extends AppCompatActivity {
     private TextView sportText;
     public DrawerLayout drawerLayout;
     private Button navButton;
+    private ImageView bingPicImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,7 +69,14 @@ public class WeatherActivity extends AppCompatActivity {
         sportText = (TextView) findViewById(R.id.sport_text);
         drawerLayout=(DrawerLayout)findViewById(R.id.draw_layout);
         navButton=(Button)findViewById(R.id.nav_button);
+        bingPicImg=(ImageView)findViewById(R.id.bing_pic_img);
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String bingPic=prefs.getString("bing_pic",null);
+        if (bingPic!=null){
+            Glide.with(this).load(bingPic).into(bingPicImg);
+        }else {
+            loadBingPic();
+        }
         String weatherString = prefs.getString("weather", null);
         final String weatherId;
         navButton.setOnClickListener(new View.OnClickListener() {
@@ -177,6 +188,34 @@ public class WeatherActivity extends AppCompatActivity {
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
 
+    }
+    /**
+     * 加载必应每日一图
+     */
+    private void loadBingPic(){
+        String requestBingPic="http://guolin.tech/api/bing_pic";
+        HttpUtil.sendOkHttpRequest(requestBingPic, new Callback() {
+
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                final String bingPic=response.body().string();
+                SharedPreferences.Editor editor=PreferenceManager.getDefaultSharedPreferences(WeatherActivity.this).edit();
+                editor.putString("bing_pic",bingPic);
+                editor.apply();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Glide.with(WeatherActivity.this).load(bingPic).into(bingPicImg);
+                    }
+                });
+            }
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
 }
